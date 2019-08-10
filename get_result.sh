0e8ac633cdb0d526df6f44c75389aec9255192ae
dreamzoy <dreamzoy@users.noreply.github.com>
2019-08-10 17:41:48 +0800

    Update get_result.sh

--- get_result.sh
+++ get_result.sh
@@ -2,8 +2,8 @@ astr=$1
 bstr=$2
 echo "=====================================Result:=========================================="
 if [ "$2" = "HBASE_22041" -o "$2" = "HBASE_" ];then
-        tail -5 /home/test/DisReproduce/logs/$2/$1
+        tail -5 ./logs/$2/$1
 else
-        grep $1 -r /home/test/DisReproduce/logs/$2/
+        grep $1 -r ./logs/$2/
 fi
 echo "======================================================================================"
